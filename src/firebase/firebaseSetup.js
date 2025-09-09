076003ca2eda5cef4313f936ab53575137389e69
PC <silveriolance06@gmail.com>
2025-09-09 22:52:53 +0800

    firebase setup

---
+++ src/firebase/firebaseSetup.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDGihnL-R88rHBxF3iTnyiUIrv4NUDdafY",
+    authDomain: "brokechef-950a6.firebaseapp.com",
+    projectId: "brokechef-950a6",
+    storageBucket: "brokechef-950a6.firebasestorage.app",
+    messagingSenderId: "379323043602",
+    appId: "1:379323043602:web:f4c558c29f770d09e66dcb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/login/loginUI.js
+++ src/login/loginUI.js
@@ -1,6 +1,6 @@
 export function loginPage() {
     const page = document.createElement('div')
-    page.innerHTML = "<h1>This is the Login Page</h1>"
+    page.innerHTML = "<h1>This is the Login Page</h1> <button><"
 
     return page
 }
