8faacf95722152c3d6a3ff0cc78b78fc1b27cc63
PC <silveriolance06@gmail.com>
2025-09-20 21:37:33 +0800

    Finalized generate view

--- src/views/homeComponents/sidebar.js
+++ src/views/homeComponents/sidebar.js
@@ -1,18 +1,24 @@
 import renderHome from "../homeView";
 
 export default function sidebar() {
-    const sidebar = document.createElement("div");
-    sidebar.id = "sidebar";
-    sidebar.className =
-        "fixed top-0 left-0 h-full w-[70%] sm:w-[400px] bg-white shadow-lg transform -translate-x-full transition-transform duration-300 ease-in-out z-50";
+  const sidebar = document.createElement("div");
+  sidebar.id = "sidebar";
+  sidebar.className =
+    "fixed top-0 left-0 h-full w-[70%] sm:w-[400px] bg-white shadow-lg transform -translate-x-full transition-transform duration-300 ease-in-out z-50";
 
-    sidebar.innerHTML = `
+  sidebar.innerHTML = `
       <div>
         <div class="flex justify-end mb-4">
           <button id="closeSidebar" class="material-icons m-4" style="font-size:35px">close</button>
         </div>
         <nav>
           <ul>
+            <li>
+              <a href="#" class="recipes flex items-center px-4 py-2 gap-3 text-2xl text-gray-700 hover:text-gray-900  hover:bg-slate-100 active:bg-slate-200">
+                <span class="material-icons mt-1.5">library_books</span>
+                <h1>Generate</h1>
+              </a>
+            </li>
             <li>
               <a href="#" class="recipes flex items-center px-4 py-2 gap-3 text-2xl text-gray-700 hover:text-gray-900  hover:bg-slate-100 active:bg-slate-200">
                 <span class="material-icons mt-1.5">library_books</span>
@@ -42,33 +48,33 @@ export default function sidebar() {
       </div>
     `;
 
-    document.addEventListener('click', (e) => {
-        // Open sidebar when menu button is clicked
-        if (e.target.matches('[data-hs-overlay="#hs-sidebar-offcanvas"]')) {
-            sidebar.classList.remove("-translate-x-full");
-        }
+  document.addEventListener('click', (e) => {
+    // Open sidebar when menu button is clicked
+    if (e.target.matches('[data-hs-overlay="#hs-sidebar-offcanvas"]')) {
+      sidebar.classList.remove("-translate-x-full");
+    }
 
-        // Close sidebar when close button is clicked
-        if (e.target.matches('#closeSidebar')) {
-            sidebar.classList.add("-translate-x-full");
-        }
-    });
+    // Close sidebar when close button is clicked
+    if (e.target.matches('#closeSidebar')) {
+      sidebar.classList.add("-translate-x-full");
+    }
+  });
 
-    //menu items selection
-    sidebar.addEventListener('click', (e) => {
-        if (e.target.classList.contains('recipes')) {
-            alert('recipe')
-        }
-        if (e.target.classList.contains('favorites')) {
-            alert('favorites')
-        }
-        if (e.target.classList.contains('search')) {
-            alert('search')
-        }
-        if (e.target.classList.contains('about')) {
-            alert('about')
-        }
-    })
+  //menu items selection
+  sidebar.addEventListener('click', (e) => {
+    if (e.target.classList.contains('recipes')) {
+      alert('recipe')
+    }
+    if (e.target.classList.contains('favorites')) {
+      alert('favorites')
+    }
+    if (e.target.classList.contains('search')) {
+      alert('search')
+    }
+    if (e.target.classList.contains('about')) {
+      alert('about')
+    }
+  })
 
-    return sidebar;
+  return sidebar;
 }
