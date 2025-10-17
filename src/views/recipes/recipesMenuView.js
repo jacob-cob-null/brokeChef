ca4e92edadfab09ce7a2c90303950be953fe48ff
PC <silveriolance06@gmail.com>
2025-10-17 21:41:54 +0800

    recipe item component added

--- src/views/recipes/recipesMenuView.js
+++ src/views/recipes/recipesMenuView.js
@@ -1,12 +1,19 @@
+import recipeItemComponent from "../../recipeItemComponent/recipeItemComponent.js"
+
 export default function recipesView() {
     const page = document.createElement('div')
-    page.className = 'flex h-full w-full flex gap-4 px-4 py-2'
+    page.className = 'flex h-full w-full flex flex-col gap-4 px-4 py-2'
 
     const header = document.createElement('h1')
     header.textContent = 'You have 8 recipes'
     header.className = 'text-3xl text-slate-700'
 
     page.appendChild(header)
+    page.appendChild(recipeItemComponent())
+    page.appendChild(recipeItemComponent())
+    page.appendChild(recipeItemComponent())
+    page.appendChild(recipeItemComponent())
+    page.appendChild(recipeItemComponent())
 
     return page
 }
