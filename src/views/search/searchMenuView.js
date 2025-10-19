c727b4118937180e74a10d472581ad6009c68475
PC <silveriolance06@gmail.com>
2025-10-19 20:47:28 +0800

    body container optimization and search menu page

---
+++ src/views/search/searchMenuView.js
@@ -0,0 +1,36 @@
+import { searchInput } from "./components/searchInput"
+import { resultContainer } from "./components/resultContainer"
+import { warning } from "../../utility/alerts"
+
+export function searchMenuView() {
+    const page = document.createElement('div')
+
+    page.className = 'h-full w-full flex flex-col gap-4 p-4'
+
+    page.innerHTML = `
+        <h1 class="text-2xl font-title text-center">Find your Recipes</h1>
+    `
+    page.appendChild(searchInput())
+    page.appendChild(resultContainer())
+
+    //append events
+    searchEvent(page)
+
+    return page
+}
+function searchEvent(root) {
+    //btn and input
+    const searchBtn = root.querySelector('#searchBtn')
+    const input = root.querySelector('#recipesField')
+
+    searchBtn.addEventListener('click', () => {
+        const value = input.value.trim()
+
+        //alert empty field
+        if (!value) {
+            warning("Recipes input field cannot be empty!")
+            return
+        }
+        alert(value)
+    })
+}
